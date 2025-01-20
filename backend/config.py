0291ca940efe275706bb1b054a9d507034a7c89a
abderrahmane beddakhe <abderrahmane.bedd@gmail.com>
2025-01-20 23:30:50 +0100

    redux setup

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,11 @@
 # backend/app/config.py
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Settings:
     MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
-    MYSQL_USER = os.getenv("MYSQL_USER", "root")
-    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "")
+    MYSQL_USER = os.getenv("MYSQL_USER", "my_user")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "my_password")
     MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "my_database")
