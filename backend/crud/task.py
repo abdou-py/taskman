155d2c1719a50a4d5dfa892f342b1cd6e3867539
abderrahmane beddakhe <abderrahmane.bedd@gmail.com>
2025-01-20 14:06:56 +0100

    added task crud operations

---
+++ backend/crud/task.py
@@ -0,0 +1,46 @@
+# backend/app/crud/task.py
+from sqlalchemy.orm import Session
+from app.models.task import Task
+from app.schemas.task import TaskCreate, Task
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def get_task(db: Session, task_id: int):
+    return db.query(Task).filter(Task.id == task_id).first()
+
+def get_tasks(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(Task).offset(skip).limit(limit).all()
+
+def create_task(db: Session, task: TaskCreate):
+    db_task = Task(**task.dict())
+    db.add(db_task)
+    db.commit()
+    db.refresh(db_task)
+    return db_task
+
+def update_task(db: Session, task_id: int, task: TaskCreate):
+    db_task = db.query(Task).filter(Task.id == task_id).first()
+    if db_task:
+        db_task.title = task.title
+        db_task.description = task.description
+        db_task.is_completed = task.is_completed
+        db.commit()
+        db.refresh(db_task)
+        return db_task
+    else:
+        return None
+
+def delete_task(db: Session, task_id: int):
+    db_task = db.query(Task).filter(Task.id == task_id).first()
+    if db_task:
+        db.delete(db_task)
+        db.commit()
+        return True
+    else:
+        return False
